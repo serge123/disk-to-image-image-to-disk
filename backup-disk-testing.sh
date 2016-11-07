195fc319f47d163aebc1fd8d5613fd1cbccd1bdc
serge123 <s.kapitonenka@gmail.com>
2016-11-07 23:43:19 +0900

    Initial commit

---
+++ backup-disk-testing.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+# Ver 0.2
+# Script to backup disk
+
+# Format files, example
+#
+# fdisk-info.txt						partitions info
+# *-mbr.bin 							MBR
+# *-mbr.bin.md5  						MBR md5sum
+# *sda1.raw.gz 							1-st partition
+# *sda1.raw.gz.md5 						1-st partition md5sum
+# *sda2.raw.gz 							2-nd partition
+# *sda2.raw.gz.md5 						2-nd partition md5sum
+# and so on
+#
+# Logical disks such as sda5, sda6 ... will be not stored separate 
+# They will be included to extended partition  
+#
+# To Do :
+# 
+
+# Const and vars
+PREFIX=/dev/
+DISK=$PREFIX$1
+POSTFIX="-"
+FILENAMEPREFIX=$2$POSTFIX
+
+MBRTXT="fdisk-info.txt"
+MBRBIN="mbr.bin"
+MBRBINMD="mbr.bin.md5"
+fdisk -l $DISK > $MBRTXT
+EXTENDED="Ext" # to recognize Extended partition in MBRTXT file
+
+RAW=".raw"
+RAWGZ=".raw.gz"
+RAWGZMD=".raw.gz.md5"
+
+# Checking input disk parameter
+if !(test -b $DISK) ; then 
+	echo "need valid disk parameter: sda, sdb or sdc"
+	echo  
+	echo "Usage: sh backup-os.sh [DISK] [FILENAME]"
+	echo "Example: sh backup-os.sh sda xpe-backup"
+	exit 0
+fi
+
+# Checking input filename parameter
+if !(test $2) ; then 
+	echo "need valid filename parameter."  
+	echo  
+	echo "Usage: sh backup-os.sh [DISK] [FILENAME]"
+	echo "Example: sh backup-os.sh sda xpe-backup"
+	exit 0
+fi
+
+# Checking that disk is not mounted
+if ( mount | grep -q $DISK ) ; then
+	mount | grep $DISK 
+	echo "Disk $DISK is mounted"
+	echo "Unmount $DISK partitions to backup $DISK"
+	exit 0	
+fi
+
+# Partitions info
+echo
+cat $MBRTXT
+echo
+
+# MBR + Partition table backup
+echo "Saving mbr sector with dd if=$DISK of=$FILENAMEPREFIX$MBRBIN bs=512 count=1"
+dd if=$DISK of=$FILENAMEPREFIX$MBRBIN bs=512 count=1
+echo "Calculating MD5 with md5sum $FILENAMEPREFIX$MBRBIN > $FILENAMEPREFIX$MBRBINMD "
+md5sum $FILENAMEPREFIX$MBRBIN > $FILENAMEPREFIX$MBRBINMD
+
+# Partitions backup
+for i in {1..9}
+do
+	if (test -b $DISK$i) ; then 
+		if ( cat $MBRTXT | grep -q $DISK$i); then
+			# Reading arguments for partition
+			PARTSTART=`cat $MBRTXT | grep $DISK$i | awk '{print $2}'`
+			# Checking that PARTSTART got Start column value, not Boot column value. Boot value is mark as *
+			if [ "$PARTSTART" == "*" ] ; then
+				PARTSTART=`cat $MBRTXT | grep $DISK$i | awk '{print $3}'`
+				PARTEND=`cat $MBRTXT | grep $DISK$i | awk '{print $4}'`
+			else
+				PARTEND=`cat $MBRTXT | grep $DISK$i | awk '{print $3}'`
+			fi
+			echo "reading partition info : start - $PARTSTART  end - $PARTEND"
+			PARTLEN=$((PARTEND-PARTSTART+1))
+			# Saving partition, compressing and calculating md5sum
+			echo "Saving partition with dd if=$DISK of=$FILENAMEPREFIX$1$i$RAW bs=64M iflag=skip_bytes,count_bytes skip=$(($PARTSTART*512)) count=$(($PARTLEN*512))"
+			dd if=$DISK of=$FILENAMEPREFIX$1$i$RAW bs=64M iflag=skip_bytes,count_bytes skip=$(($PARTSTART*512)) count=$(($PARTLEN*512))
+			echo "Compressing partition with gzip -9 $FILENAMEPREFIX$1$i$RAW"
+			gzip -9 $FILENAMEPREFIX$1$i$RAW
+			echo "Calculating MD5 with md5sum $FILENAMEPREFIX$1$i$RAWGZ > $FILENAMEPREFIX$1$i$RAWGZMD"
+			md5sum $FILENAMEPREFIX$1$i$RAWGZ > $FILENAMEPREFIX$1$i$RAWGZMD
+			#Exiting if partition is extended
+			if ( cat $MBRTXT | grep $DISK$i | grep $EXTENDED ); then
+				echo "Extended partition detected: $DISK$i"
+				break
+			fi 		
+		fi
+	fi
+done
+
+ls -l | grep $FILENAMEPREFIX
+
+echo "Finished .."
---
+++ installing-disk-testing.sh
@@ -0,0 +1,159 @@
+#!/bin/sh
+#
+# Ver 0.2
+# Script to restore disk
+#
+# Format files, example
+#
+# fdisk-info.txt 						partitions info
+# *-mbr.bin 							MBR
+# *-mbr.bin.md5  						MBR md5sum
+# *sda1.raw.gz 							1-st partition
+# *sda1.raw.gz.md5 						1-st partition md5sum
+# *sda2.raw.gz 							2-nd partition
+# *sda2.raw.gz.md5 						2-nd partition md5sum
+# and so on
+#
+# To Do :
+# 
+
+# Const and vars
+CURDIR=`ls`
+
+PREFIX=/dev/
+SEARCHPART="/dev/..."
+DISK=$PREFIX$1
+
+MBRTXT="fdisk-info.txt"
+MBRBIN="mbr.bin"
+MBRBINMD="mbr.bin.md5"
+
+EXCLUD=md5
+
+RAWGZ=".raw.gz"
+RAWGZMD=".raw.gz.md5"
+
+# Checking input disk parameter
+if !(test -b $DISK) ; then 
+	echo "need valid disk parameter: sda, sdb or sdc"  
+	echo  
+	echo "Usage: sh installing-os.sh [DISK]"
+	echo "Example: sh installing-os.sh sda" 
+	exit 0
+fi
+
+if !(test -f $MBRTXT) ; then	
+	echo "file $MBRTXT is not found"
+	echo "Exiting .."
+	exit 0
+fi
+
+# Checking that disk is not mounted
+if ( mount | grep -q $DISK ) ; then
+	mount | grep $DISK 
+	echo "Disk $DISK is mounted"
+	echo "Unmount $DISK partitions to restore $DISK"
+	exit 0	
+fi
+
+# Partitions info
+echo
+cat $MBRTXT
+echo
+
+# Writing MBR + Partition table 
+for FILENAME in $CURDIR
+do
+	if (echo $FILENAME | grep -v $EXCLUD | grep -q $MBRBIN) ; then	
+
+		echo "Found $FILENAME, seaching for MD5SUM file..."
+
+		# Checking md5sum
+		MD5FILE="NOT FOUND"
+		for FILENAMEMD5 in $CURDIR
+		do			
+			if (echo $FILENAMEMD5 | grep -q $MBRBINMD) ; then
+				MD5FILE="FOUND"
+				echo "$FILENAMEMD5 found"
+				echo "Checking MD5SUM of $FILENAME with $FILENAMEMD5"
+				MD5SUMF=`cat $FILENAMEMD5`
+				MD5SUMCHECK=`md5sum $FILENAME`
+				if [ "$MD5SUMF" == "$MD5SUMCHECK" ] ; then
+					echo "MD5SUM is OK."
+					echo "dd if=$FILENAME of=$DISK bs=512 count=1"
+					dd if=$FILENAME of=$DISK bs=512 count=1
+					break
+				else 
+					echo "MD5SUM is diffierent. $FILENAME is corrupted."
+					echo "Exiting .."
+					exit 0
+				fi
+			fi
+		done
+
+		if [ "$MD5FILE" == "NOT FOUND" ] ; then
+			echo "MD5SUM file for $FILENAME is not found. "
+			echo "Exiting .."
+			exit 0
+		fi
+		echo 
+		break
+
+	fi
+
+done
+
+# Writing partitions
+for i in {1..9}
+do
+
+	for FILENAME in $CURDIR
+	do
+		if (echo $FILENAME | grep -v $EXCLUD | grep -q $i$RAWGZ) ; then
+		
+			echo "Found $FILENAME, seaching for MD5SUM file..."
+			
+			# Checking md5sum
+			MD5FILE="NOT FOUND"
+			for FILENAMEMD5 in $CURDIR
+			do			
+				if (echo $FILENAMEMD5 | grep -q $i$RAWGZMD) ; then
+					MD5FILE="FOUND"
+					echo "$FILENAMEMD5 found"
+					echo "Checking MD5SUM of $FILENAME with $FILENAMEMD5"
+					MD5SUMF=`cat $FILENAMEMD5`
+					MD5SUMCHECK=`md5sum $FILENAME`
+					if [ "$MD5SUMF" == "$MD5SUMCHECK" ] ; then
+						echo "MD5SUM is OK."					
+						# Reading argument for partition
+						PARTSTART=`cat $MBRTXT | grep "$SEARCHPART$i" | awk '{print $2}'`
+						# Checking that PARTSTART got Start column value, not Boot column value. Boot value is mark as * 							
+						if [ "$PARTSTART" == "*" ] ; then
+							PARTSTART=`cat $MBRTXT | grep "$SEARCHPART$i" | awk '{print $3}'`
+						fi
+						echo "reading partition info : start - $PARTSTART "
+						echo "gunzip -c $FILENAME | dd of=$DISK bs=64M oflag=seek_bytes seek=$(($PARTSTART* 512))"
+						gunzip -c $FILENAME | dd of=$DISK bs=64M oflag=seek_bytes seek=$(($PARTSTART* 512))
+						break
+					else 
+						echo "MD5SUM is diffierent. $FILENAME is corrupted."
+						echo "Exiting .."
+						exit 0
+					fi
+				fi
+			done
+			
+			if [ "$MD5FILE" == "NOT FOUND" ] ; then
+				echo "MD5SUM file for $FILENAME is not found. "
+				echo "Partition will be not restored"
+				echo "Exiting .."
+				exit 0
+			fi
+			echo 
+			break
+		fi
+	done
+
+done
+
+echo "Finished, restart PC .."
